44e81ed43869bffb0b1a270d78c09d634d2699a2
bhagya0896 <77100278+bhagya0896@users.noreply.github.com>
2021-01-18 13:15:12 +0530

    Update script2.js

--- script2.js
+++ script2.js
@@ -155,6 +155,39 @@ var obj = fromListToObject(array);
 
 console.log(obj);
 
+ //Problem 6 : Parsing a list of lists and convert into a JSON object
+
+ var array = [
+  [
+    ['firstName', 'Vasanth'],
+    ['lastName', 'Raja'], 
+    ['age', 24], 
+    ['role', 'JSWizard']
+  ], 
+  [
+    ['firstName', 'Sri'], 
+    ['lastName', 'Devi'], 
+    ['age', 28], 
+    ['role', 'Coder']
+  ]
+];
+
+function transformEmployeeData(arr) {
+  var tranformEmployeeList = [];
+  for (var i= 0; i< arr.length; i++){
+    let newObject = {};	
+    for (var j= 0; j< arr[i].length; j++){
+      let key = arr[i][0];
+      let value = arr[i][1];
+      newObject[key] = value;	
+    }
+    tranformEmployeeList.push(newObject);
+  }
+  return tranformEmployeeList;
+}
+
+console.log(transformEmployeeData(array));          
+
 
 
 
